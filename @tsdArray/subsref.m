1cc48538195d0a4e8dbf717b764d5a7154d45fd9
Luke <lukesjulson@gmail.com>
2017-08-19 17:11:23 -0400

    Fixed subsref() in tsdArray so that everything is indexed correctly when one uses dot syntax to call methods from objects in the array.

--- @tsdArray/subsref.m
+++ @tsdArray/subsref.m
@@ -1,14 +1,10 @@
-function B = subsref(A, S)
-% overload of operator {} for element referencing.
-
-% if subscripts are
-% numieric, the correspondign elements of the array are returned, if
-% subscript is a string, all the tsd's are returned that contain that
-% string in their name
+function varargout = subsref(A, S)
+% overload of operators for element referencing.
 %
-% copyright (c) 2004 Francesco P. Battaglia
-% This software is released under the GNU GPL
-% www.gnu.org/copyleft/gpl.html
+% () indexing returns a new tsdArray composed of elements of the original.
+% {} indexing can only be used for a single tsdArray element, and it
+%    returns the tsd contained within the tsdArray
+%
 %
 % v2.0, Luke Sjulson, Aug 2017
 % TODO: add back in text-based indexing
@@ -16,12 +12,20 @@ function B = subsref(A, S)
 
 if strcmp(S(1).type, '{}')
    %     disp('subsref called');
-   if length(S) > 1
-      error('subsref defined only for one level of subscripting');
+   %    if length(S) > 1
+   %       error('subsref defined only for one level of subscripting');
+   %    end
+   
+   if length(S(1).subs)>1
+      error('subsref does not yet support greater than 1-D indexing of tsdArrays');
    end
    
-   if length(S.subs{:})>1
+   if length(S(1).subs{:})>1
       error('If you index a tsdArray with curly braces, you can only index one element of the array');
+   end
+   
+   if ~isnumeric(S(1).subs{1})
+      error('curly brace indexing must use a number');
    end
    
    V = ver;
@@ -31,54 +35,74 @@ if strcmp(S(1).type, '{}')
       warning('Versions of matlab prior to R2015b do not index tsdArrays correctly. Time to upgrade.')
    end
    
-   B = A.C{S.subs{:}};
-   
-   % % below is the old code, which implemented indexing by text search. Should
-   %    % incorporate at some point
-   %     num_subs = 1;
-   %     for i = 1:length(S.subs)
-   %         if ~isa(S.subs{i}, 'numeric')
-   %             num_subs = 0;
-   %         end
-   %     end
-   %
-   %     if num_subs
-   %         B = A.C{S.subs{:}};
-   %         return
-   %     elseif length(S.subs) == 1
-   %         if ischar(S.subs{1})
-   %             k = S.subs{1};
-   %             ix = [];
-   %             for i = 1:length(A.C)
-   %                 n = Name(A.C);
-   %                 if length(n) >= length(k)
-   %                     if ~isempty(findstr(n, k))
-   %                         ix = [ix i];
-   %                     end
-   %                 end
-   %
-   %             end
-   %             B = A.C{ix};
-   %             return
-   %         end
-   %     end
-   %
-   %     error(lasterr);
-   
-   
-elseif strcmp(S(1).type, '()')
-   if length(S) > 1
-      error('subsref defined only for one level of subscripting');
-   end
-   B = A.C(S.subs{:});
-   B = tsdArray(B);
-   
-   % for calling methods
-elseif strcmp(S(1).type, '.')
+   % accessing single TS/TSD in the array
    if length(S)==1
-      B = feval(S.subs, A);
-   elseif length(S) > 1 && strcmp(S(2).type,'()')
-      B = feval(S(1).subs, A, S(2).subs);
+      varargout{1} = A.C{S.subs{1}};
+      
+      % calling method or getting property, no arguments
+   elseif length(S)==2 && strcmp(S(2).type,'.')
+      
+      if isprop(A.C{S(1).subs{1}}, S(2).subs) % it's a property, not a method
+         eval(['varargout{1} = A.C{S(1).subs{1}}.' S(2).subs ';']);
+      else % it's a method
+         [varargout{1:nargout(S(2).subs)}] = feval(S(2).subs, A.C{S(1).subs{1}});
+      end
+      
+      % calling property or method with arguments
+   elseif length(S)==3 && strcmp(S(2).type,'.') && strcmp(S(3).type, '()')
+      if isprop(A.C{S(1).subs{1}}, S(2).subs) % it's a property, not a method
+         if length(S(3).subs)>1
+            error('subsref can not handle indexing more than one dimension');
+         else
+            eval(['varargout{1} = A.C{S(1).subs{1}}.' S(2).subs '([' num2str(S(3).subs{1}) ']);']);
+         end
+      else % it's a method
+         [varargout{1:nargout(S(2).subs)}] = feval(S(2).subs, A.C{S(1).subs{1}}, S(3).subs{:});
+      end
+      
+   else
+      error('subsref can not handle that many levels of indexing');
+   end
+   
+elseif strcmp(S(1).type, '()') % for indexing subarrays of the whole array
+   
+   if length(S) > 1
+      error('subsref defines only for one level of subscripting for ()');
+   end
+   
+   if length(S(1).subs) > 1
+      error('indexing more than one dimension with () not supported');
+   end
+   
+   if isnumeric(S(1).subs{1})
+      varargout{1} = tsdArray(A.C(S.subs{:}));
+      
+   elseif isstring(S(1).subs{1}) % string indexing by searching name property
+      error('string indexing not yet implemented');
+      
+   elseif islogical(S(1).subs{1})
+      varargout{1} = tsdArray(A.C(S.subs{:}));
+      
+   else error('subsref can only index with numbers or logical arrays');
+      
+   end
+   
+elseif strcmp(S(1).type, '.') % for calling tsdArray methods using dot syntax
+   
+   if length(S)==1 % no arguments
+      if all(isprop(A, S(1).subs)) % it's a property
+         eval(['varargout{1} = A.' S(1).subs ';']);
+      else % it's a method
+         [varargout{1:nargout(S(1).subs)}] = feval(S(1).subs, A);
+      end
+      
+   elseif length(S) > 1 && strcmp(S(2).type,'()') % property/method gets arguments
+      if all(isprop(A, S(1).subs)) % it's a property
+         error('subsref does not support indexing properties of tsdArrays');
+      else % it's a method
+         [varargout{1:nargout(S(1).subs)}] = feval(S(1).subs, A, S(2).subs);
+      end
+      
    else error('subsref does not handle that type of indexing');
    end
 end
